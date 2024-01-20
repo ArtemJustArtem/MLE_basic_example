c53ea0e4dbb05e38eea2713015eb64ed65db817c
ArtemJustArtem <ashkyl97@gmail.com>
2024-01-20 15:33:02 +0200

    Added data saving

--- data_process/data_generation.py
+++ data_process/data_generation.py
@@ -48,11 +48,14 @@ class DataGenerator():
         y = iris['target']
         feature_names = iris['feature_names']
         X_train, X_inference, y_train, _ = train_test_split(X, y, test_size=inference_size, random_state=random_state, stratify=y)
+        pd.DataFrame(X_train, columns=feature_names).to_csv(X_train_path, index=False)
+        pd.DataFrame(y_train, columns=['Target']).to_csv(y_train_path, index=False)
+        pd.DataFrame(X_inference, columns=feature_names).to_csv(inference_path, index=False)
 
 # Main execution
 if __name__ == "__main__":
     configure_logging()
-    logger.info("Starting script...")
+    logger.info("Starting loading data...")
     gen = DataGenerator()
-    gen.load_data(None, None, None, conf['inference']['inference_size'], conf['general']['random_state'])
-    logger.info("Script completed successfully.")
+    gen.load_data(X_TRAIN_PATH, Y_TRAIN_PATH, INFERENCE_PATH, conf['inference']['inference_size'], conf['general']['random_state'])
+    logger.info("Data loaded successfully.")
