dee0cc1bd1ba9ab0258c782f2f69622a898729f7
ArtemJustArtem <ashkyl97@gmail.com>
2024-01-20 18:17:03 +0200

    Add data training and evaluating

--- training/train.py
+++ training/train.py
@@ -1,6 +1,8 @@
 """
 This script prepares the data, runs the training, and saves the model.
 """
+import numpy as np
+import pandas as pd
 
 import argparse
 import os
@@ -51,12 +53,14 @@ parser.add_argument("--batch_size", type=int,
                     default=32)
 parser.add_argument("--epochs", type=int,
                     help="Number of epochs used during training",
-                    default=25)
+                    default=100)
+args = parser.parse_args()
 
 def data_pipeline(X_train_path, y_train_path, test_size, random_state):
     X = pd.read_csv(X_train_path)
     y = pd.read_csv(y_train_path)['Target']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state, stratify=y)
+    y_encoded = pd.get_dummies(y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=test_size, random_state=random_state, stratify=y)
     X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
     X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
     y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32)
@@ -66,25 +70,58 @@ def data_pipeline(X_train_path, y_train_path, test_size, random_state):
     return train_dataset, test_dataset
 
 class NNModel(nn.Module):
-    def __init__(self, hidden_neurons):
+    def __init__(self, hidden_neurons, device):
         super(NNModel, self).__init__()
+        self.device = device
         neurons = hidden_neurons.split(',')
         self.hidden_layers = []
         in_features = 4
         for n in neurons:
             out_features = int(n)
-            self.hidden_layers.append(nn.Linear(in_features=in_features, out_features=out_features, bias=True))
+            self.hidden_layers.append(nn.Linear(in_features=in_features, out_features=out_features, bias=True).to(self.device))
             in_features = out_features
-        self.final_layer = nn.Linear(in_features=in_features, out_features=3, bias=True)
-        self.relu = nn.ReLU()
-        self.softmax = nn.Softmax()
+        self.final_layer = nn.Linear(in_features=in_features, out_features=3, bias=True).to(self.device)
+        self.relu = nn.ReLU().to(self.device)
+        self.softmax = nn.Softmax(dim=1).to(self.device)
 
     def forward(self, x):
+        x = x.to(self.device)
         for layer in self.hidden_layers:
             x = self.relu(layer(x))
         return self.softmax(self.final_layer(x))
 
+def evaluate_model(model, dataset):
+    model.eval()
+    with torch.no_grad():
+        X, y = dataset[:][0], dataset[:][1]
+        y = torch.tensor(np.argmax(y.cpu().numpy(), axis=1)).type(torch.int)
+        predictions = torch.tensor(np.argmax(model(X).cpu().numpy(), axis=1)).type(torch.int)
+        return multiclass_accuracy(predictions, y).item()
+    
+def train_model(model, train_dataset, batch_size, epochs):
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    for epoch in range(epochs):
+        model.train()
+        for X, y in train_dataloader:
+            optimizer.zero_grad()
+            output = model(X)
+            loss = criterion(output, y.to(model.device))
+            loss.backward()
+            optimizer.step()
+    return model
+
 
 if __name__ == "__main__":
     configure_logging()
-    _ = NNModel("8,4,2")
+    np.random.seed(conf['general']['random_state'])
+    torch.random.manual_seed(conf['general']['random_state'])
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(conf['general']['random_state'])
+        torch.backends.cudnn.benchmark = False
+    train_dataset, test_dataset = data_pipeline(X_TRAIN_PATH, Y_TRAIN_PATH, conf['train']['test_size'], conf['general']['random_state'])
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = NNModel(args.hidden_neurons, device)
+    trained_model = train_model(model, train_dataset, args.batch_size, args.epochs)
+    
