53aeb480a5637bfe11361122e9f86c8b8c5ba155
ArtemJustArtem <ashkyl97@gmail.com>
2024-01-20 16:53:02 +0200

    Added data pipeline

--- training/train.py
+++ training/train.py
@@ -11,9 +11,12 @@ import logging
 import pandas as pd
 import time
 from datetime import datetime
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
+
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from torch import nn
+from torcheval.metrics.functional import multiclass_accuracy
 
 # Comment this lines if you have problems with MLFlow installation
 import mlflow
@@ -35,95 +38,35 @@ with open(CONF_FILE, "r") as file:
 # Defines paths
 DATA_DIR = get_project_dir(conf['general']['data_dir'])
 MODEL_DIR = get_project_dir(conf['general']['models_dir'])
-TRAIN_PATH = os.path.join(DATA_DIR, conf['train']['table_name'])
+X_TRAIN_PATH = os.path.join(DATA_DIR, conf['train']['X_train_name'])
+Y_TRAIN_PATH = os.path.join(DATA_DIR, conf['train']['y_train_name'])
 
 # Initializes parser for command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--train_file", 
-                    help="Specify inference data file", 
-                    default=conf['train']['table_name'])
-parser.add_argument("--model_path", 
-                    help="Specify the path for the output model")
+parser.add_argument("--hidden_neurons", type=str,
+                    help="Number of neurons in each hidden layer (integers separated by comma)", 
+                    default="8,4")
+parser.add_argument("--batch_size", type=int,
+                    help="Batch size used during training",
+                    default=32)
+parser.add_argument("--epochs", type=int,
+                    help="Number of epochs used during training",
+                    default=25)
 
+def data_pipeline(X_train_path, y_train_path, test_size, random_state):
+    X = pd.read_csv(X_train_path)
+    y = pd.read_csv(y_train_path)['Target']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state, stratify=y)
+    X_train_tensor = torch.tensor(X_train.values, dtype=torch.float32)
+    X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
+    y_train_tensor = torch.tensor(y_train.values, dtype=torch.float32)
+    y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32)
+    train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+    test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+    return train_dataset, test_dataset
 
-class DataProcessor():
-    def __init__(self) -> None:
-        pass
-
-    def prepare_data(self, max_rows: int = None) -> pd.DataFrame:
-        logging.info("Preparing data for training...")
-        df = self.data_extraction(TRAIN_PATH)
-        df = self.data_rand_sampling(df, max_rows)
-        return df
-
-    def data_extraction(self, path: str) -> pd.DataFrame:
-        logging.info(f"Loading data from {path}...")
-        return pd.read_csv(path)
-    
-    def data_rand_sampling(self, df: pd.DataFrame, max_rows: int) -> pd.DataFrame:
-        if not max_rows or max_rows < 0:
-            logging.info('Max_rows not defined. Skipping sampling.')
-        elif len(df) < max_rows:
-            logging.info('Size of dataframe is less than max_rows. Skipping sampling.')
-        else:
-            df = df.sample(n=max_rows, replace=False, random_state=conf['general']['random_state'])
-            logging.info(f'Random sampling performed. Sample size: {max_rows}')
-        return df
-
-
-class Training():
-    def __init__(self) -> None:
-        self.model = DecisionTreeClassifier(random_state=conf['general']['random_state'])
-
-    def run_training(self, df: pd.DataFrame, out_path: str = None, test_size: float = 0.33) -> None:
-        logging.info("Running training...")
-        X_train, X_test, y_train, y_test = self.data_split(df, test_size=test_size)
-        start_time = time.time()
-        self.train(X_train, y_train)
-        end_time = time.time()
-        logging.info(f"Training completed in {end_time - start_time} seconds.")
-        self.test(X_test, y_test)
-        self.save(out_path)
-
-    def data_split(self, df: pd.DataFrame, test_size: float = 0.33) -> tuple:
-        logging.info("Splitting data into training and test sets...")
-        return train_test_split(df[['x1','x2']], df['y'], test_size=test_size, 
-                                random_state=conf['general']['random_state'])
-    
-    def train(self, X_train: pd.DataFrame, y_train: pd.DataFrame) -> None:
-        logging.info("Training the model...")
-        self.model.fit(X_train, y_train)
-
-    def test(self, X_test: pd.DataFrame, y_test: pd.DataFrame) -> float:
-        logging.info("Testing the model...")
-        y_pred = self.model.predict(X_test)
-        res = f1_score(y_test, y_pred)
-        logging.info(f"f1_score: {res}")
-        return res
-
-    def save(self, path: str) -> None:
-        logging.info("Saving the model...")
-        if not os.path.exists(MODEL_DIR):
-            os.makedirs(MODEL_DIR)
-
-        if not path:
-            path = os.path.join(MODEL_DIR, datetime.now().strftime(conf['general']['datetime_format']) + '.pickle')
-        else:
-            path = os.path.join(MODEL_DIR, path)
-
-        with open(path, 'wb') as f:
-            pickle.dump(self.model, f)
-
-
-def main():
-    configure_logging()
-
-    data_proc = DataProcessor()
-    tr = Training()
-
-    df = data_proc.prepare_data(max_rows=conf['train']['data_sample'])
-    tr.run_training(df, test_size=conf['train']['test_size'])
 
 
 if __name__ == "__main__":
-    main()
+    configure_logging()
+    _, _ = data_pipeline(X_TRAIN_PATH, Y_TRAIN_PATH, conf['train']['test_size'], conf['general']['random_state'])
